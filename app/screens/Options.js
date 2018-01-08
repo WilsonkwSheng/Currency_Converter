19d9f84f4ea32af963758773f8bd1cb01d324c29
wilson <wilsonkhoo93@gmail.com>
2018-01-08 09:54:59 +0800

    setup error alert using dropdownalert

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-
-import { ListItem, Seperator } from '../components/List'
+import { ListItem, Seperator } from '../components/List';
+import { connectAlert } from '../components/Alert';
 
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 const ICON_COLOR = '#868686';
@@ -12,6 +12,7 @@ const ICON_SIZE = 23;
 class Options extends Component {
     static propTypes = {
         navigation: PropTypes.object,
+        alertWithType: PropTypes.func,
     };
 
     handleThemePress = () => {
@@ -19,7 +20,7 @@ class Options extends Component {
     };
 
     handleSitePress = () => {
-        Linking.openURL('http://fixer.io').catch(() => alert('An error occured'));
+        Linking.openURL('http://fixer.io').catch(() => this.props.alertWithType('error', 'Sorry!', "Fixer.io can't be opened right now."));
     };
 
     render () {
@@ -47,4 +48,4 @@ class Options extends Component {
     }
 }
 
-export default Options;
+export default connectAlert(Options);
